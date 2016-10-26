dd51036e9cd29ead71fb1b6451df5c7c1f06daa7
Nauman Leghari <nauman.leghari@wipro.com>
2016-10-26 16:09:54 +0100

    Redirecting output to stdout instead of devnull

--- install.sh
+++ install.sh
@@ -5,27 +5,27 @@
 
 if [[ -z $(which brew) ]]; then
   echo "Installing Homebrew...";
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /dev/null;
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 fi
 
 if [[ -z $(which ansible) ]]; then
     echo "Installing Ansible";
-    brew install ansible > /dev/null;
+    brew install ansible;
 fi
 
 WHOAMI=$(whoami);
 
 if [[ -d "/Users/${WHOAMI}/Documents/dotfiles" ]]; then
     echo "Removing dotfiles";
-    rm -rf "/Users/${WHOAMI}/Documents/dotfiles" > /dev/null;
+    rm -rf "/Users/${WHOAMI}/Documents/dotfiles";
 fi
 if [[ -d "/Users/${WHOAMI}/.setup" ]]; then
     echo "Removing playbook";
-    rm -rf "/Users/${WHOAMI}/.setup" > /dev/null;
+    rm -rf "/Users/${WHOAMI}/.setup";
 fi
 
-git clone https://github.com/nauman-leghari-wipro/mac-dev-playbook.git "/Users/${WHOAMI}/.setup" > /dev/null;
-git clone https://github.com/nauman-leghari-wipro/mac-dev-playbook-dotfiles.git "/Users/${WHOAMI}/Documents/dotfiles" > /dev/null;
+git clone https://github.com/nauman-leghari-wipro/mac-dev-playbook.git "/Users/${WHOAMI}/.setup";
+git clone https://github.com/nauman-leghari-wipro/mac-dev-playbook-dotfiles.git "/Users/${WHOAMI}/Documents/dotfiles";
 
 if [[ -n $1 ]] && [[ -n $2 ]]; then
     EXTRAVARS="app_store_email=$1 app_store_password=$2";
